896ff28b2422a789da7096b3c98a4da574491b6e
Yoon Min <minusxix@gmail.com>
2024-08-07 20:28:18 +0700

    demo

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
+import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 app.secret_key = '#$#$#$#$#$#$'
@@ -7,13 +10,74 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:%s@localhost/ecommerce'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+train_data = pd.read_csv("models/clean_data.csv")
+
+# class Signup(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(100), nullable=False)
+#     username = db.Column(db.String(100), nullable=False)
+#     password = db.Column(db.String(100), nullable=False)
+#
+#
+# class Signin(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(100), nullable=False)
+#     password = db.Column(db.String(100), nullable=False)
 
 
+def content_based_recommendations(train_data, item_name, top_n=10):
+    # Check if the item name exists in the training data
+    if item_name not in train_data['Name'].values:
+        print(f"Item '{item_name}' not found in the training data!")
+        return pd.DataFrame()
+
+    # Create a TF-IDF vectorizer for item descriptions
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+
+    # Apply TF-IDF vectorization to item descriptions
+    tfidf_matrix_content = tfidf_vectorizer.fit_transform(train_data['Tags'])
+
+    # Calculate cosine similarity between items based on descriptions
+    cosine_similarities_content = cosine_similarity(tfidf_matrix_content, tfidf_matrix_content)
+
+    # Find the index of the item
+    item_index = train_data[train_data['Name'] == item_name].index[0]
+
+    # Get the cosine similarity scores for the item
+    similar_items = list(enumerate(cosine_similarities_content[item_index]))
+
+    # Sort similar items by similarity score in descending order
+    similar_items = sorted(similar_items, key=lambda x:x[1], reverse=True)
+
+    # Get the top N most similar items (excluding the item itself)
+    top_similar_items = similar_items[1:top_n+1]
+
+    # Get the indices of the top similar items
+    recommended_item_indices = [x[0] for x in top_similar_items]
+
+    # Get the details of the top similar items
+    recommended_items_details = train_data.iloc[recommended_item_indices][['Name', 'ReviewCount', 'Brand', 'ImageURL', 'Rating']]
+
+    return recommended_items_details
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', content_based_rec=None)
+
+
+@app.route("/recommendations", methods=['POST', 'GET'])
+def recommendations():
+    if request.method == 'POST':
+        prod = request.form.get('prod')
+        nbr = int(request.form.get('nbr'))
+        content_based_rec = content_based_recommendations(train_data, prod, top_n=nbr)
+
+        if content_based_rec.empty:
+            message = "No recommendations available for this product!"
+            return render_template('index.html', message=message)
+
+        return render_template('index.html', content_based_rec=content_based_rec)
 
 
 if __name__ == '__main__':
